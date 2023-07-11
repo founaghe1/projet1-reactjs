693abd8183bc9d588895d1e1fe91eca2102871c3
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-07-11 18:05:15 +0000

    First Commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import './main.css'
+
+// array nammed data
+
+// const Data = [
+//     {
+//         id: 1,
+//         imgSrc: img,
+//         destination: 'Bora Bora',
+//         location: 'New Zealand',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$700',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 2,
+//         imgSrc: img2,
+//         destination: 'Machu Picchu',
+//         location: 'Peru',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$600',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 3,
+//         imgSrc: img3,
+//         destination: 'Great Barrier Reef',
+//         location: 'Australia',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$700',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 4,
+//         imgSrc: img4,
+//         destination: 'Cappadocia',
+//         location: 'Turkey',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$800',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 5,
+//         imgSrc: img5,
+//         destination: 'Guanajuato',
+//         location: 'Mexico',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$1100',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 6,
+//         imgSrc: img6,
+//         destination: 'Cinque Terre',
+//         location: 'Italy',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$850',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 7,
+//         imgSrc: img7,
+//         destination: 'Angkor wat',
+//         location: 'Cambodia',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$790',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 8,
+//         imgSrc: img8,
+//         destination: 'Taj Mahal',
+//         location: 'India',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$900',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+//     {
+//         id: 9,
+//         imgSrc: img9,
+//         destination: 'Bali Island',
+//         location: 'Indonesia',
+//         grade: 'CULTURAL RELAX',
+//         fees: '$500',
+//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex perspiciatis iure quo quis enim nemo, eveniet, nobis nisi qui inventore at ipsam sed corporis facere eius incidunt velit rerum esse.'
+//     },
+
+// ]
+
+const Main =() =>{
+    return(
+        <section className="main container section">
+            <div className="secTitle">
+                <h3 className="title">
+                    Most visited destinations
+                </h3>
+            </div>
+
+            <div className="secContent grid">
+
+            </div>
+        </section>
+    )
+}
+
+export default Main;
